e603d78f5e0dfd9a22c03275319b892796e8ab7b
Kasra Tabrizi <kasra_tabrizi@hotmail.com>
2022-03-07 16:13:54 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <div className={styles.header__container}>
       <h1>Where in the world?</h1>
-      <div className={styles.theme__toggler}>Dark Mode</div>
+      <div className={styles.theme__toggler}>
+        <ion-icon name="moon-outline"></ion-icon> <span>Dark Mode</span>
+        {/* <ion-icon name="moon"></ion-icon>Dark Mode */}
+      </div>
     </div>
   );
 };
