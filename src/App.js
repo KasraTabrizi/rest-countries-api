057901488ac5d9600e44b2951c6d2da7cdfa4c58
Kasra Tabrizi <kasra_tabrizi@hotmail.com>
2022-03-15 15:27:19 +0100

    filter region en error finished

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ import { useFetchCountries } from "./hooks/useFetchAll";
 
 function App() {
   const [countries, error, loading, setFetchFilter] = useFetchCountries();
-  console.log(countries);
   const [searchInput, setSearchInput] = useState("");
 
-  const onChangeHandler = (e) => {
+  const onSearchCountryHandler = (e) => {
     setSearchInput(e.target.value);
-    console.log(e.target.value);
     if (!e.target.value) {
       setFetchFilter("none", ""); //if input is empty again, show all countries
     } else {
       setFetchFilter("country", e.target.value); //if something typed in input, filter for country
+    }
+  };
+
+  const onFilterRegionHandler = (e) => {
+    if (e.target.value === "default") {
+      setFetchFilter("none", ""); //if input is empty again, show all countries
+    } else {
+      setFetchFilter("region", e.target.value); //if something typed in input, filter for region
     }
   };
 
@@ -36,18 +42,32 @@ function App() {
           type="text"
           value={searchInput}
           placeholder="Search for a country..."
-          onChange={(e) => onChangeHandler(e)}
+          onChange={(e) => onSearchCountryHandler(e)}
         />
       </div>
-      <div></div>
+      <div>
+        <select
+          name="region"
+          id="region"
+          onChange={(e) => onFilterRegionHandler(e)}
+        >
+          <option value="default">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
       <div className="coutrylist__container">
         {loading ? (
-          <div>loading</div>
-        ) : (
-          countries &&
+          <div>loading...</div>
+        ) : countries ? (
           countries.map((country, index) => {
             return <Country country={country} key={index} id={index} />;
           })
+        ) : (
+          error && <div>error</div>
         )}
       </div>
     </div>
