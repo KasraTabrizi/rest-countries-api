89509a649276b66488a2a545312fb6951179a61a
Kasra Tabrizi <kasra_tabrizi@hotmail.com>
2022-03-21 18:16:19 +0100

    t

--- src/components/CountryList/Country/Country.jsx
+++ src/components/CountryList/Country/Country.jsx
@@ -17,60 +17,62 @@ const Country = ({ country, onClickHandler, expandCountryInfo }) => {
       <div className={styles.country_info__container}>
         <h3>{country.name.common}</h3>
 
-        {expandCountryInfo && (
-          <p>
-            <span>Native Name: </span>
-            {country.name.nativeName &&
-              country.name.nativeName[
-                Object.keys(country.name.nativeName)[
-                  Object.keys(country.name.nativeName).length - 1
-                ]
-              ].common}
-          </p>
-        )}
-
-        <p>
-          <span>Population:</span> {country.population}
-        </p>
-        <p>
-          <span>Region:</span> {country.region}
-        </p>
-        {expandCountryInfo && (
-          <p>
-            <span>Sub Region:</span> {country.subregion}
-          </p>
-        )}
-        <p>
-          <span>Capital:</span> {country.capital}
-        </p>
-
-        {expandCountryInfo && (
-          <div className={styles.expanded_info__container}>
+        <div className={styles.desktop_info__container}>
+          <div>
+            {expandCountryInfo && (
+              <p>
+                <span>Native Name: </span>
+                {country.name.nativeName &&
+                  country.name.nativeName[
+                    Object.keys(country.name.nativeName)[
+                      Object.keys(country.name.nativeName).length - 1
+                    ]
+                  ].common}
+              </p>
+            )}
             <p>
-              <span>Top Level Domain:</span> {country.tld}
+              <span>Population:</span> {country.population}
             </p>
             <p>
-              <span>Currencies: </span>
-              {country.currencies &&
-                country.currencies[
-                  Object.keys(country.currencies)[
-                    Object.keys(country.currencies).length - 1
-                  ]
-                ].name}
+              <span>Region:</span> {country.region}
             </p>
+            {expandCountryInfo && (
+              <p>
+                <span>Sub Region:</span> {country.subregion}
+              </p>
+            )}
             <p>
-              <span>Languages: </span>
-              {country.languages &&
-                Object.keys(country.languages).map((lang, index) => {
-                  return `${country.languages[lang]}${
-                    index < Object.keys(country.languages).length - 1
-                      ? ", "
-                      : ""
-                  }`;
-                })}
+              <span>Capital:</span> {country.capital}
             </p>
           </div>
-        )}
+          {expandCountryInfo && (
+            <div className={styles.expanded_info__container}>
+              <p>
+                <span>Top Level Domain:</span> {country.tld}
+              </p>
+              <p>
+                <span>Currencies: </span>
+                {country.currencies &&
+                  country.currencies[
+                    Object.keys(country.currencies)[
+                      Object.keys(country.currencies).length - 1
+                    ]
+                  ].name}
+              </p>
+              <p>
+                <span>Languages: </span>
+                {country.languages &&
+                  Object.keys(country.languages).map((lang, index) => {
+                    return `${country.languages[lang]}${
+                      index < Object.keys(country.languages).length - 1
+                        ? ", "
+                        : ""
+                    }`;
+                  })}
+              </p>
+            </div>
+          )}
+        </div>
 
         {expandCountryInfo && (
           <div className={styles.expanded_info__container}>
